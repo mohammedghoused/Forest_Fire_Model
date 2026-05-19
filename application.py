653e57587ec72ed3ea7c911e81f003f93a6747b2
mdghouse <ghouse.ashrafi47@gmail.com>
2026-05-19 13:00:28 +0530

    added template [index.html]

--- application.py
+++ application.py
@@ -4,3 +4,17 @@ import pandas as pd
 import sklearn.preprocessing import StandardScaler
 
 application = Flask(__name__)
+app = application
+
+#importing ridge regressor and standard scaler pickle
+ridge_model=pickle.load(open('models/ridge.pkl','wb'))
+standard_scaler=pickle.load(open('models/scaler.pkl','wb'))
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
+    
