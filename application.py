33bd6f8bce01332d148485802780a052000a7c23
mdghouse <ghouse.ashrafi47@gmail.com>
2026-05-19 13:11:49 +0530

    added home page

--- application.py
+++ application.py
@@ -1,20 +1,26 @@
 from flask import Flask,request,jsonify,render_template
 import numpy as np
 import pandas as pd
-import sklearn.preprocessing import StandardScaler
+import pickle
+from sklearn.preprocessing import StandardScaler
 
 application = Flask(__name__)
 app = application
 
 #importing ridge regressor and standard scaler pickle
-ridge_model=pickle.load(open('models/ridge.pkl','wb'))
-standard_scaler=pickle.load(open('models/scaler.pkl','wb'))
+ridge_model=pickle.load(open('models/ridge.pkl','rb'))
+standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
 
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+@app.route("/predictdata",methods=['GET','POST'])
+def predict_datapoint():
+    if request.method =="post":
+        pass
+    else:
+        return render_template('home.html')
 if __name__=="__main__":
     app.run(host="0.0.0.0")
-    
